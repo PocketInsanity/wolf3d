97bb58455c6d36fb7a71c0ca10dee232456d5feb
Steven Fuller <relnev@icculus.org>
2000-05-06 21:01:36 +0000

    Prepared makefile, time to start the port

--- macsrc/HideMenuBar.h
+++
@@ -1,4 +0,0 @@
-extern void HideMenuBar(void);
-extern void ShowMenuBar(void);
-extern void FixMHeight(void);
-extern void ZapMHeight(void);
--- macsrc/Prefs.h
+++
@@ -1,3 +0,0 @@
-void InitPrefsFile(OSType creator,Byte *PrefsName);
-OSErr LoadPrefsFile(Byte *PrefsPtr,Word PrefsLen);
-OSErr SavePrefsFile(Byte *PrefsPtr,Word PrefsLen);
